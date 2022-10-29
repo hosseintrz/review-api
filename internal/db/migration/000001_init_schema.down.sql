157b1cf6ef0ec99d9c4b4d4e4a40f4025bf44a0a
hosseintrz <htrzit@gmail.com>
2022-10-29 22:49:38 +0330

    added db migrations

--- internal/db/migration/000001_init_schema.down.sql
+++ internal/db/migration/000001_init_schema.down.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS suggestions;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS movies;
--- internal/db/migration/000001_init_schema.up.sql
+++ internal/db/migration/000001_init_schema.up.sql
@@ -1,18 +1,30 @@
 CREATE TABLE "users" (
-  "id" bigserial PRIMARY KEY,
-  "username" varchar NOT NULL,
-  "password" varchar NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT (now())
+                         "id" bigserial PRIMARY KEY,
+                         "username" varchar NOT NULL,
+                         "password" varchar NOT NULL,
+                         "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE "suggestions" (
-  "id" bigserial PRIMARY KEY,
-  "user_id" bigserial NOT NULL,
-  "text" varchar
+CREATE TABLE "movies" (
+                          "id" bigserial PRIMARY KEY,
+                          "name" varchar NOT NULL,
+                          "year" integer NOT NULL,
+                          "director" varchar,
+                          "rating" numeric(2,1)
+);
+
+CREATE TABLE "reviews" (
+                               "id" bigserial PRIMARY KEY,
+                               "user_id" bigserial NOT NULL,
+                               "movie_id" bigserial NOT NULL,
+                               "rating" numeric(2,1),
+                               "text" varchar
 );
 
 CREATE INDEX ON "users" ("username");
 
-CREATE INDEX ON "suggestions" ("user_id");
+CREATE INDEX ON "reviews" ("user_id", "movie_id");
 
-ALTER TABLE "suggestions" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "reviews" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "reviews" ADD FOREIGN KEY ("movie_id") REFERENCES "movies" ("id");
